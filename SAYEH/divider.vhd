8b9e53227e388fe80ca968f0f3e132a7fab32a47
sina <s.malakouti2015@aut.ac.ir>
2017-04-23 23:31:11 +0430

    fixing bugs in random and divider

--- SAYEH/divider.vhd
+++ SAYEH/divider.vhd
@@ -11,37 +11,42 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity div_binary is
 Port (
 
-            ina : in std_logic_vector (15 downto 0);-- range 0 to 99;
-          inb:  in std_logic_vector (7 downto 0);-- range 1 to 9;
-         quot:  out std_logic_vector (7 downto 0);-- range 0 to 99;
+ina : in std_logic_vector (15 downto 0);-- range 0 to 99;
+inb:  in std_logic_vector (7 downto 0);-- range 1 to 9;
+quot:  out std_logic_vector (15 downto 0)
+-- range 0 to 99;
+--      rest : out std_logic_vector (15 downto 0));-- range 0 to 99
+);
 
 end div_binary;
 
 architecture Behavioral of div_binary is
 
-signal a,b: integer range 0 to 65535;
+signal a: integer range 0 to 65535;
+signal b : integer range 0 to 255;
 begin
 
 a <= CONV_INTEGER(ina);
 b <= CONV_INTEGER(inb);
 process (a,b)
 
-variable temp1,temp2: integer range 0 to 65535;
+variable temp1: integer range 0 to 65535;
+variable temp2 : integer range 0 to 255;
 variable y :  std_logic_vector (15 downto 0);
- begin
- temp1:=a;
- temp2:=b;
- for i in 15 downto 0 loop
- if (temp1>temp2 * 2**i) then
- y(i):= '1';
- temp1:= temp1- temp2 * 2**i;
- else y(i):= '0';
- end if;
- end loop;
- rest <= CONV_STD_LOGIC_VECTOR (temp1 ,16);
-  quot<= y;
- --quot<= conv_integer (y);
-  end process;
+begin
+temp1:=a;
+temp2:=b;
+for i in 15 downto 0 loop
+if (temp1>=temp2 * 2**i) then
+y(i):= '1';
+temp1:= temp1- temp2 * 2**i;
+else y(i):= '0';
+end if;
+end loop;
+--rest <= CONV_STD_LOGIC_VECTOR (temp1 ,16);
+quot<= y;
+--quot<= conv_integer (y);
+end process;
 
 
-end Behavioral
+end Behavioral;
--- SAYEH/random .vhd
+++ SAYEH/random .vhd
@@ -1,22 +1,24 @@
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 
 entity random is
-  port (
-  a  : in std_logic_vector(15 downto 0);
-  result : out std_logic_vector(15 downto 0)
-  );
+port (
+a  : in std_logic_vector(15 downto 0);
+result : out std_logic_vector(15 downto 0)
+);
 end entity;
 
 architecture arch of random is
 
 signal temp : std_logic_vector(15 downto 0);
 
-
 begin
-temp <= a srl 1;
-temp(15) <= a(0) xor a(7) xor a(15);
-
-random <= temp;
+--temp(0) <= '0';
+temp(15 downto 1) <= a(14 downto 0);
+temp(0) <= a(0) xor a(7) xor a(15);
+result <= temp;
 end architecture;
+
+
